663f2cabe418c32b467a36a523a688b68629e1aa
fpt-huypt13 <96227275+fpt-huypt13@users.noreply.github.com>
2022-03-22 22:15:55 +0700

    x

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const mongo = require("./src/database/connect");
 const studentRouter = require("./src/student/student.routes");
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 require("dotenv").config();
